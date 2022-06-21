b54c9b5a7995e1b394ba7471565a3cdbf9ffee4a
Elliot (aka. Proliecan) <65495509+Proliecan@users.noreply.github.com>
2022-06-21 23:28:18 +0200

    Haskell Hello World

---
+++ haskell/README.md
@@ -0,0 +1,4 @@
+# Setup
+
+
+Go to https://www.haskell.org/ghcup and follow the instructions to download Haskell.
---
+++ haskell/helloWorld/helloWorld.hs
@@ -0,0 +1,6 @@
+message = "Hello World!" :: [Char];
+
+-- print the message to the screen
+main = do
+    print message; :: IO ()
+
